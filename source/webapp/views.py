ca9249823fbeb03a0921fd139030afdf476f4b1b
AVMoldashev <vasilykryakov3@gmail.com>
2024-11-27 21:22:27 +0600

    created fixtures

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponseRedirect
-from webapp.articleDB import ArticleDB
+from webapp.models import Article
 # Create your views here.
 
 
 def index_view(request):
     context = {
-        'articles': ArticleDB.articles
+        'articles': Article.objects.all()
     }
     return render(request, 'index.html', context=context)
 
@@ -15,11 +15,17 @@ def article_create_view(request):
     if request.method == 'GET':
         return render(request, 'article_create.html')
     elif request.method == 'POST':
-        article = {
-            'title': request.POST.get('title'),
-            'content': request.POST.get('content'),
-            'author': request.POST.get('author')
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        author = request.POST.get('author')
+        article = Article.objects.create(title=title, content=content, author=author)
+        context = {
+            'article': article
         }
-        ArticleDB.articles.append(article)
-        return HttpResponseRedirect("/")
+        return render(request, 'article_view.html', context)
+
+def article_view(request):
+    article_id = request.GET.get('id')
+    article = Article.objects.get(id=article_id)
+    return render(request, 'article_view.html', context={'article': article})
 
