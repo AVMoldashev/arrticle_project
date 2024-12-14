6d2c7ecde447dd17d9ba6e7d87bd602e87dd364c
AVMoldashev <vasilykryakov3@gmail.com>
2024-12-14 22:04:01 +0600

    form

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.forms import widgets
+from webapp.models import Tag
 
 class ArticleForm(forms.Form):
     title = forms.CharField(max_length=50, required=True, label="Название")
     content = forms.CharField( required=True, label="Текст", widget=widgets.Textarea)
     author = forms.CharField(max_length=50, required=True, label="Автор")
+    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), label="Теги", required=True)
